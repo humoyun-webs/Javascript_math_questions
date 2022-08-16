5812050d7e82c95176c63a3b922b5befa6326aa3
humoyun-webs <humoyuneshpolatov5@gmail.com>
2022-08-16 10:17:02 -0800

    javascript math question

---
+++ script.js
@@ -0,0 +1,209 @@
+// #1
+// let k = 10
+// let n = 5
+
+// for( let k = 10 ; n = 5; result = k * n){
+//     console.log(result);
+// }
+
+
+
+
+
+// #2
+// let a = 10
+// let b = 5
+// for(b = 5; b<=a; b++ ){
+// console.log(b);
+// }
+
+
+// #3
+// let b = 5
+// for (let a = 15; a > b; a--) {
+//    if (a=== 15)
+//       continue
+//    console.log(a);
+// }
+
+
+
+
+// #4
+// for(i=10000; i<=100000; i+=10000){
+//     console.log(i+"kg")
+// }
+
+
+
+
+// #5
+// for(i=1000; i<=10000; i+=1000){
+//     console.log(i+"kg")
+// }
+
+
+
+// #6
+// for(i=10000; i<=100000; i+=2000){
+//     console.log(i+"kg")
+// }
+
+
+// #7
+
+// const number =+ prompt('Son kirgizing: ');
+
+// let sum = 1;
+
+// for (let i = 1; i <= number; i++) {
+//     sum += i;
+// }
+
+// console.log('1dan aytgan songizgacha bolgan sonlarni yigindisi:', sum);
+
+
+
+
+// #8
+// const number =+ prompt('Son kirgizing: ');
+
+// let sum = 1;
+
+// for (let i = 1; i <= number; i++) {
+//     sum *= i;
+// }
+
+// console.log('1dan aytgan songizgacha bolgan sonlarni kopaytmasi:', sum);
+
+
+
+
+// #9
+// const number =+ prompt("Son kirgizing")
+
+// for (let a = 1; a <= number; a++) {
+//     console.log(`1dan aytgan songizgacha bolgan sonlarni kvadrati:${a**2}`);
+// }
+
+
+
+
+// #10
+
+// const number =+ prompt("Son kirgizing")
+// for (let n = 1; n <= number; n++){
+
+//     console.log(`Aytgan soningizgizcha bu formula qaytarildi: ${1/n + 1/n}`);
+    
+// } 
+
+
+
+
+// #11
+// const number =+ prompt("Son kirgizing")
+
+// for (let i = number; i <= number ; i++){
+//     if(i % 2 == 0){
+//     console.log("Juft son");
+//     }else{
+//         console.log("Toq son");
+//     }
+// }
+
+
+
+// #12
+// const number =+ prompt("Son kirgizing")
+
+// for (let i = number; i <= number ; i++){
+//     if(i % 2 == 0){
+//     console.log("Juft son");
+//     }else{
+//         console.log("Toq son");
+//     }
+// }
+
+
+
+
+
+// #13
+// const n =+ prompt("Son kirgizing")
+
+// for (let i = 1; i <= n ; i++) {
+//     if((n % 2 ==0)) {
+//         console.log("murakkab sonlar:" + i);
+//     }else{
+//         console.log("Ushbu son murakkab emas " + i);
+//     }
+      
+    
+    
+// }
+// chunmadim
+
+
+
+
+
+// #14
+// const number =+ prompt("Son kirgizing")
+
+// for (let i = 1; i <= number ; i++){
+//     if(i % 2 == 0){
+//         i = number * number
+//     console.log("Juft sonni kvadratga oshirilganda: " + i);
+//     }else{
+//         console.log("Toq sonni kvadratga oshirilganda: " + i);
+//     }
+//     console.log(i);
+// }
+
+
+
+
+// #15
+// for(let i=10; i>=1; i--){    
+//     console.log(i);
+// }
+
+
+
+// #16
+// const n =+ prompt("Son kirgizing")
+// for (let i = 1; i <= n; i++){
+//     if(i % 5 == 0 )
+//     continue
+//     console.log(i);
+// }
+
+
+
+
+
+
+// #17
+// const number =+ prompt('Son kirgizing: ');
+
+// let sum = 1;
+
+// for (let i = 1; i <= number; i++) {
+//     sum *= i;
+// }
+
+// console.log('1dan aytgan songizgacha bolgan sonlarni kopaytmasi:', sum);
+
+
+
+// kara jadval
+// for(let i =1; i<=10; i++){
+//     for (let j = 1; j <= 10; j++){
+//         document.write(`${i} x ${j} = ${i*j} <br>`)
+//     }
+// }
+
+
+
+
